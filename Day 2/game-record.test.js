67401d38e349c57b9901a5a1196774ef13c5f9f1
Joseph Henry Passineau <joseph.passineau@ariv.ca>
2023-12-03 23:57:22 -0500

    Day 2 fix
    Correct names of some tests

--- Day 2/game-record.test.js
+++ Day 2/game-record.test.js
@@ -6,7 +6,7 @@ test.each([
     ['Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 3, 3],
     ['Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 4, 3],
     ['Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', 5, 2],
-])('game set %s should have %i red balls, %i green balls and %i blue balls', (line, expectedId, expectedSetsCount) => {
+])('game record %s should have id %i and %i total sets', (line, expectedId, expectedSetsCount) => {
     const gameRecord = new GameRecord(line);
     expect(gameRecord.Id).toBe(expectedId);
     expect(gameRecord.Sets.length).toBe(expectedSetsCount);
--- Day 2/game-statistics.test.js
+++ Day 2/game-statistics.test.js
@@ -7,7 +7,7 @@ test.each([
     ['Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 1560],
     ['Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 630],
     ['Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', 36],
-])('game set %s should return power of %i', (line, expectedPowerOfFewestNeededCubes) => {
+])('game statistics with game %s should return power of %i', (line, expectedPowerOfFewestNeededCubes) => {
     const gamesStatistics = new GamesStatistics();
     gamesStatistics.addGameRecord(new GameRecord(line));
     expect(gamesStatistics.calculatePowerOfFewestNeededCubes()).toBe(expectedPowerOfFewestNeededCubes);
